85bdaada56f6a5f991b2d5a02f35539684f2882a
pavanandra <pavansk7075@gmail.com>
2025-03-05 23:34:48 +0530

    Add AWS provider configuration and Terraform backend settings

--- 1.provider.tf
+++ 1.provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "<= 1.8.5" #Forcing which version of Terraform needs to be used
+  required_version = "<= 1.11.1" #Forcing which version of Terraform needs to be use
   required_providers {
     aws = {
       version = "<= 6.0.0" #Forcing which version of plugin needs to be used.
@@ -11,7 +11,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "workspacesbucket01"
+    bucket         = "pavankibucket"
     key            = "Ansible.tfstate"
     region         = "us-east-1"
     # dynamodb_table = "-terraform-locks"
